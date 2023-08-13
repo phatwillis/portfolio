a4a8ce0ec54587a710dd93bb1bae02eccaa6007a
Michael Williams <59102284+phatwillis@users.noreply.github.com>
2023-08-13 21:13:54 +0100

    Update main.js

--- script/main.js
+++ script/main.js
@@ -5,3 +5,23 @@ AOS.init({
   });
   
   // Add your javascript here
+
+  // Add this JavaScript after including AOS and other libraries
+  document.addEventListener('DOMContentLoaded', function() {
+    var projectsDropdownToggle = document.querySelector('#projectsDropdown');
+    var projectsDropdownMenu = document.querySelector('.dropdown-menu');
+
+    projectsDropdownToggle.addEventListener('click', function() {
+      projectsDropdownMenu.classList.toggle('show');
+    });
+
+    // Close the dropdown when clicking outside of it
+    window.addEventListener('click', function(event) {
+      if (!projectsDropdownToggle.contains(event.target) && !projectsDropdownMenu.contains(event.target)) {
+        projectsDropdownMenu.classList.remove('show');
+      }
+    });
+  });
+
+
+
